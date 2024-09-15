1d53b66a87c31fa4c92343de26411a545ab18e9e
kudosmax <escriboy@icloud.com>
2024-09-15 15:46:03 +0900

    categorizing

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { getPostBySlug } from "@/app/lib/api";
 import markdownToHtml from "@/app/lib/markdownToHtml";
 import { notFound } from "next/navigation";
+import { format } from "date-fns";
 
 export default async function BlogPost({
   params,
@@ -17,9 +18,15 @@ export default async function BlogPost({
 
   return (
     <article className="max-w-2xl mx-auto mt-4 sm:mt-6 md:mt-10">
-      <h1 className="text-3xl font-bold mb-4">{post.frontmatter.title}</h1>
+      <h1 className="text-3xl font-bold mb-2">{post.frontmatter.title}</h1>
+      <div className="text-sm text-gray-500 mb-4">
+        {format(new Date(post.frontmatter.date), "MMMM d, yyyy")}
+      </div>
+      <div className="flex justify-center items-center my-12">
+        <span className="text-gray-400 text-md inline-block">/</span>
+      </div>
       <div
-        className="prose dark:prose-invert"
+        className="prose dark:prose-invert mt-8"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </article>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
@@ -6,15 +7,44 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class", // 이 줄을 추가합니다
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      typography: ({ theme }: { theme: (path: string) => string }) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.900"),
+            a: {
+              color: theme("colors.blue.600"),
+              "&:hover": {
+                color: theme("colors.blue.800"),
+              },
+            },
+            "h1, h2, h3, h4": {
+              color: theme("colors.gray.900"),
+              "margin-top": "1.5em",
+              "margin-bottom": "0.5em",
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.100"),
+            a: {
+              color: theme("colors.blue.400"),
+              "&:hover": {
+                color: theme("colors.blue.300"),
+              },
+            },
+            "h1, h2, h3, h4": {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [typography],
 };
+
 export default config;
